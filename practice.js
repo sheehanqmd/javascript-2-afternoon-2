7a3efe81d48174c5c18024813ef6714f577460b4
donna <sheehanqmd@gmail.com>
2019-05-03 13:22:36 -0500

    day2-part2

--- practice.js
+++ practice.js
@@ -18,6 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return (arr[0])
+}
 
 
 
@@ -33,7 +36,10 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return(arr[2])
+  
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +54,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper (family){
+  for(let i= 0; i< family.length; i++)
+  alert(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,6 +72,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+ function reversedLooper(letters){
+  for (var i = letters.length - 1; i >= 0; i--)
+  alert(letters[i])
+ }
 
 
 
@@ -78,6 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(numbers){
+  var evens = []
+  for(var i=0; i < numbers.length; i++ ) {
+    if(numbers[i]%2===0) {
+      evens.push(numbers[i]) 
+    }
+  }  
+  return evens 
+}
 
 
 
